4b40e05f47cfc1d61cdf2afcf8a793e5af9c0e50
unknown <myzhoulang>
2015-11-04 00:02:24 +0800

    改动

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var articles = require('./routes/articles');
+var articles = require('./routes/articles')
 
 var app = express();
 
